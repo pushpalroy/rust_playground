f0bb42c1f14362b39aa0c269d05537d14c2c8c25
Pushpal Roy <pushpalroy2007@gmail.com>
2024-02-01 14:51:27 -0600

    [learn]: fn visibility and mod

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
+mod visibility_mod;
+
+fn function() {
     println!("Hello, this is a playground for Rust!");
+}
+
+fn main() {
+    // Calling a function
+    function();
+
+    // Cannot call a private function
+    // visibility_mod::my_mod::private_function();
+
+    // Calling a public function in a different mod
+    visibility_mod::my_mod::function();
+
+    // Calling a public function which calls another function internally
+    visibility_mod::my_mod::indirect_access();
+
+    // Calling a nested mod
+    visibility_mod::my_mod::nested::function();
 }
